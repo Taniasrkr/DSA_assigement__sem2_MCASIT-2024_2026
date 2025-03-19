261be2a7cdddae35f5a566e7860de5bf2f870e4c
Taniya Sarkar <145034710+Taniasrkr@users.noreply.github.com>
2025-03-19 16:46:38 +0530

    Add files via upload

---
+++ assigment/1_e.py
@@ -0,0 +1,16 @@
+
+# Write a function that takes a string as a parameter and returns a string
+# with every successive repetitive character replaced with a star(*). For
+# example, ‘balloon’ is returned as ‘bal*o*n’.
+
+str=input("enter text : ")
+dict={}
+
+def replace_char(str):
+    for i in range(len(str)-1):
+        if str[i] in dict:
+            str=str[:i]+ "*" + str[i+1:]
+        dict[str[i]]=i
+    return str
+
+print(replace_char(str))
